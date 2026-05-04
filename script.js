f2e405e77c0b503eacd50f969efc2e7e0fc0ef31
Arthur Vinícius M. <99282591+Arthurvdev@users.noreply.github.com>
2026-05-04 17:14:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const btn_buscar = document.querySelector('#btn_buscar');
+const input_nome = document.querySelector('#nome');
+const div_conteiner1 = document.querySelector('.conteiner_1');
+const div_conteiner3 = document.querySelector('.conteiner_3');
+
+const gif = document.querySelector('#gif_site');
+const title = document.querySelector('#title_site');
+
+
+input_nome.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        buscar();
+    }
+});
 
 function dados_na_tela(dados){
     document.querySelector('#nome_tela').innerHTML = dados[0].name;
@@ -16,6 +30,10 @@ async function pesquisar(nome) {
 }
 
 function buscar(){
+    title.style.display = 'none';
+    gif.style.display = 'none';
+    div_conteiner1.style.height = '10vh';
+    div_conteiner3.style.display = 'flex';
     nome = document.getElementById('nome').value;
     pesquisar(nome);
 }
